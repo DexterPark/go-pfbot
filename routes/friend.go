6891792fbe43b11331ca882fed206e34cca2bf77
iamchanii <iamchanii-@naver.com>
2018-02-19 19:04:35 +0900

    플러스친구 API 작성 및 주석 작성

--- routes/friend.go
+++ routes/friend.go
@@ -3,10 +3,12 @@ package routes
 import "github.com/gin-gonic/gin"
 import "net/http"
 
+// FriendAddedRoute 는 친구 추가 시 사용합니다.
 func FriendAddedRoute(c *gin.Context) {
 	c.String(http.StatusOK, "Hello")
 }
 
+// FriendRemovedRoute 는 친구 삭제(차단) 시 사용합니다.
 func FriendRemovedRoute(c *gin.Context) {
 	c.String(http.StatusOK, "Hello")
 }
--- routes/home_keyboard.go
+++ routes/home_keyboard.go
@@ -1,8 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/iamchanii/go-gin-kakaopf-api-example/pf"
+)
+
+// HomeKeyboardRoute 는 최초 시작 시 응답하는 라우트입니다.
 func HomeKeyboardRoute(c *gin.Context) {
-	c.String(http.StatusOK, "Hello")
+	c.JSON(http.StatusOK, pf.Keyboard(nil))
 }
